6716e15a4b95d959ea6a2705235dd8d45f3d20e8
savannah8 <willengitonga.wg@gmail.com>
2019-04-29 15:33:52 +0300

    Auto-generated commit

--- src/app/time-lapse.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TimeLapsePipe } from './time-lapse.pipe';
-
-describe('TimeLapsePipe', () => {
-  it('create an instance', () => {
-    const pipe = new TimeLapsePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
